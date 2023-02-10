b77753e7d3dbffa47a475315aba7ec6737a7dcad
Sudhakar Swain <91896763+sudhakar-swain-au27@users.noreply.github.com>
2023-02-10 09:07:56 +0530

    skygoal backend

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.listen(4000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server Started Successfully.");
+  }
+});
+
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb+srv://sudhakar143:VQXoobmEqTexW0ID@cluster0.isjtflu.mongodb.net/JWT?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+app.use(express.json());
+app.use("/", authRoutes);
